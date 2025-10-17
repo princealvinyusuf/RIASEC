b2b29fac10410ce219f1a2f7d54916aef66203c2
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2025-10-17 07:06:31 +0700

    Update logo tab

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Riasec Test</title>
+    <link rel="icon" href="jobi.png" type="image/png">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
