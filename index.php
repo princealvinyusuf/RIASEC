52eb1679fe12c8b8f259214ab7e3ec9b31553e25
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2026-09-21 09:43:06 +0700

    Hide button "Masuk Admin"

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
     <div class="cta-row">
       <a href="personal_info" class="btn btn-primary-soft">Mulai Asesmen</a>
       <a href="survei_evaluasi" class="btn btn-outline-soft">Survei Evaluasi</a>
-      <a href="admin_login" class="btn btn-outline-soft">Masuk Admin</a>
     </div>
     <div class="steps-grid">
       <div class="step-box">
